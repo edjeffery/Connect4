749a8b92ef077cded59482776d9fb75788dff964
Ed Jeffery <ej301@bath.ac.uk>
2018-01-12 10:47:28 +0000

    Spelling error corrected

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
 		System.out.println("Welcome to Connect 4");
 		System.out.println("There are 2 players red and yellow");
 		System.out.println("Player 1 is Red, Player 2 is Yellow");
-		System.out.println("To play the game type in the number of the column you want to drop you counter in");
+		System.out.println("To play the game type in the number of the column you want to drop your counter in");
 		System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally");
 		System.out.println("");
 		
